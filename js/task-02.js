318583ca0ab3a55671995834dd96b6f3da871d52
OleksandrR61 <a80953031761@gmail.com>
2022-10-07 19:21:09 +0300

    Complite task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+document.querySelector("#ingredients").append(...ingredients.map(element => {
+  const elementOfList = document.createElement('li');
+  elementOfList.textContent = element;
+  elementOfList.classList.add("item");
+  return elementOfList;
+}));
